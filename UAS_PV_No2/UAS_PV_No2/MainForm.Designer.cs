bef40b17113ca122c4b1435d1e7207bf9fb36496
Salsabilabalqis14 <86075019+Salsabilabalqis14@users.noreply.github.com>
2022-06-09 14:11:21 +0700

    Add files via upload

---
+++ UAS_PV_No2/UAS_PV_No2/MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: hp
+ * Date: 09/06/2022
+ * Time: 12:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace UAS_PV_No2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.label6 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.total = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(218, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(311, 40);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Pemesanan Makanan";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(298, 231);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "Ya";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(300, 287);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 2;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Cash";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(298, 129);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(290, 28);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(124, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Menu";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(124, 180);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Jumlah";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(298, 180);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(113, 26);
+			this.numericUpDown1.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(124, 230);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Member";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(124, 278);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(111, 55);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Metode Pembayaran";
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(410, 287);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(203, 24);
+			this.radioButton2.TabIndex = 9;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "ATM/Bank Transfer";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(29, 32);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(181, 23);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Total Pembayaran";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Controls.Add(this.total);
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Location = new System.Drawing.Point(124, 423);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(511, 87);
+			this.panel1.TabIndex = 11;
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(198, 32);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(28, 23);
+			this.label7.TabIndex = 11;
+			this.label7.Text = ":";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(300, 352);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(127, 35);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "Hitung";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// total
+			// 
+			this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.total.Location = new System.Drawing.Point(232, 32);
+			this.total.Name = "total";
+			this.total.Size = new System.Drawing.Size(201, 23);
+			this.total.TabIndex = 12;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(781, 561);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "UAS_PV_No2";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label total;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ UAS_PV_No2/UAS_PV_No2/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UAS_PV_No2
+{
+	public partial class MainForm : Form
+	{
+		double harga, total_bayar, jumlah_pesan;
+		double persen_diskon, Diskon, pajak;
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			Menu();
+		}
+		
+		void Menu()
+		{
+			comboBox1.Items.Add("Nasi Goreng");
+			comboBox1.Items.Add("Bakso Spesial");
+			comboBox1.Items.Add("Bakso Biasa");
+			comboBox1.Items.Add("Ayam Geprek");
+			comboBox1.Items.Add("Ayam Bakar");
+		}
+		
+		void Harga()
+		{
+			if (comboBox1.Text == "Nasi Goreng")
+			{
+				harga = 12000;
+			}
+			else if (comboBox1.Text == "Bakso Spesial")
+			{
+				harga = 18000;
+			}
+			else if (comboBox1.Text == "Bakso Biasa")
+			{
+				harga = 10000;
+			}
+			else if (comboBox1.Text == "Ayam Geprek")
+			{
+				harga = 20000;
+			}
+			else if (comboBox1.Text == "Ayam Bakar")
+			{
+				harga = 25000;
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			Harga();
+			
+			jumlah_pesan = Int16.Parse(numericUpDown1.Text); 
+			
+			total_bayar = harga * jumlah_pesan;
+			
+			if (checkBox1.Checked)
+			{
+				persen_diskon = 0.05;
+				Diskon = harga * jumlah_pesan * persen_diskon;
+				total_bayar = (harga * jumlah_pesan) - Diskon;
+			}
+			
+			if (radioButton1.Checked)
+			{
+				pajak = 0;
+			}
+			else if (radioButton2.Checked)
+			{
+				pajak = 5000;
+			}
+			
+			total_bayar = total_bayar - pajak;
+			
+			total.Text = total_bayar.ToString();
+			
+		}
+		
+	}
+}
